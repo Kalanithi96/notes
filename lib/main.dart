ff0d5880608a267af3cef9bb1b7e549822f47173
Kalanithi J P <kalanithi96@gmail.com>
2023-10-30 15:16:50 +0530

    Renamed create new view to create or update note view - reusing the same view for update as well

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes/constants/routes.dart';
 import 'package:notes/services/auth/auth_service.dart';
-import 'package:notes/views/notes/new_note_view.dart';
+import 'package:notes/views/notes/create_update_note_view.dart';
 import 'package:notes/views/verify_email.dart';
 import 'package:notes/views/login_view.dart';
 import 'package:notes/views/notes/notes_view.dart';
@@ -20,7 +20,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     )
     );

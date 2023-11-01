d8b49ff37de9aea63e1bcabc5c85507895a3d68f
Kalanithi J P <kalanithi96@gmail.com>
2023-11-01 22:36:35 +0530

    Reverting the deletion of SQLite CRUD service

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,21 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class DatabaseIsNotOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class UserDoesNotExist implements Exception {}
+
+class UserMismatch implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
+
+class UserMustBeSetBeforeReadingAllNotes implements Exception {}
